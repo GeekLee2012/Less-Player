f92bec36efae8a2e127d0b0574599d40c57265bd
RIVE2012 <Rive08@126.com>
2022-01-24 21:06:02 +0800

    Add files via upload

---
+++ Less-Player-Api/src/xyz/less/util/AudioUtil.java
@@ -0,0 +1,39 @@
+package xyz.less.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Paths;
+
+import javax.sound.sampled.AudioFormat;
+
+import xyz.less.bean.Audio;
+
+public final class AudioUtil {
+	
+	public static double bytes2Minutes(long bytes, AudioFormat format) {
+		int channs = format.getChannels();
+		double rate = format.getSampleRate();
+		double sampleSizeInByte = format.getSampleSizeInBits() / 8;
+		double bps = channs * rate * sampleSizeInByte;
+		double secs = bytes / bps;
+		double minutes = secs / 60D;
+//		System.out.println("Minutes: " + StringUtil.toMmss(minutes));
+		return minutes;
+	}
+	
+	public static File getAudioFile(Audio audio) {
+		return Paths.get(URI.create(audio.getSource())).toFile();
+	}
+	
+	public static InputStream getInputStream(Audio audio) {
+		try {
+			return new FileInputStream(getAudioFile(audio));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
