baf61ce043581ed3ccd2024bef4acef25ddc84f6
RIVE2012 <Rive08@126.com>
2022-01-25 15:26:05 +0800

    Delete src/xyz/less directory

--- src/xyz/less/graphic/handler/IDndHandle.java
+++
@@ -1,9 +0,0 @@
-package xyz.less.graphic.handler;
-
-import xyz.less.graphic.control.DndAction.DndContext;
-
-public interface IDndHandle {
-	DndContext getContext();
-	void handle(DndContext context);
-	IDndHandle addHandleAction(IDndHandleAction action);
-}
--- src/xyz/less/graphic/handler/IDndHandleAction.java
+++
@@ -1,8 +0,0 @@
-package xyz.less.graphic.handler;
-
-import xyz.less.graphic.control.DndAction.DndContext;
-
-@FunctionalInterface
-public interface IDndHandleAction {
-	void handle(DndContext context);
-}
