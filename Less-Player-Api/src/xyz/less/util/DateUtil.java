e301a7f5a38fcef7a71c5c3f3e5d5e738f3f73ab
RIVE2012 <rive08@126.com>
2022-03-04 15:55:04 +0800

    代码完善

--- Less-Player-Api/src/xyz/less/util/DateUtil.java
+++ Less-Player-Api/src/xyz/less/util/DateUtil.java
@@ -9,12 +9,16 @@ public final class DateUtil {
 			StringTokenizer tokenizer = new StringTokenizer(mmssSSS);
 			String mm = StringUtil.trim(tokenizer.nextToken(":"));
 			String ss = StringUtil.trim(tokenizer.nextToken("."))
-									.substring(1);
-			String SSS = StringUtil.trim(tokenizer.nextToken());
-			
+					.substring(1);
 			int minutes = Integer.parseInt(mm);
 			int secords = Integer.parseInt(ss);
-			int millis = Integer.parseInt(SSS);
+			int millis = 0;
+
+			if(mmssSSS.indexOf(".") != -1) {
+				String SSS = StringUtil.trim(tokenizer.nextToken());
+				millis = Integer.parseInt(SSS);
+			}
+
 			return minutes + (secords + millis / 1000.0D) / 60.0D;
 		} catch (Exception e) {
 			e.printStackTrace();
